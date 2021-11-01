f5bbbed10dac3bb25d0337e70eeb342d8b03e1b2
Sebastian Webber <sebastian@swebber.me>
2021-11-01 18:51:03 -0300

    chore: bump pg version to 14
    Signed-off-by: Sebastian Webber <sebastian@swebber.me>

--- pkg/defaults/pg.go
+++ pkg/defaults/pg.go
@@ -2,8 +2,8 @@ package defaults
 
 const (
 	// PGVersion is the current stable version of PostgreSQL
-	PGVersion = "13"
+	PGVersion = "14"
 
 	// PGVersionF is the current stable version of PostgreSQL - on the float Format
-	PGVersionF = 13.1
+	PGVersionF = 14.0
 )
