6f5a8947a9a9be0a095f4aca02314ecac64dcb2c
Sebastian Webber <sebastian@swebber.me>
2022-04-22 19:20:36 -0300

    chore: add byte parse support
    Signed-off-by: Sebastian Webber <sebastian@swebber.me>

--- pkg/config/bytes.go
+++ pkg/config/bytes.go
@@ -3,6 +3,9 @@ package config
 import (
 	"bytes"
 	"fmt"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 // IEC Sizes
@@ -27,19 +30,30 @@ func (b *Byte) MarshalJSON() ([]byte, error) {
 
 // String converts bytes into human bytes
 func (b *Byte) String() string {
-	return formatBytes(*b)
+	return FormatBytes(*b)
+}
+
+func (b *Byte) Set(v string) error {
+	val, err := Parse(v)
+	*b = val
+
+	return err
+}
+
+func (b *Byte) Type() string {
+	return "Byte"
 }
 
 func marshalBytes(b *Byte) ([]byte, error) {
 
 	buffer := bytes.NewBufferString("")
-	buffer.WriteString(fmt.Sprintf(`"%s"`, formatBytes(Byte(*b))))
+	buffer.WriteString(fmt.Sprintf(`"%s"`, FormatBytes(Byte(*b))))
 
 	return buffer.Bytes(), nil
 }
 
 // ideas from https://github.com/dustin/go-humanize/blob/master/bytes.go#L68
-func formatBytes(i Byte) string {
+func FormatBytes(i Byte) string {
 
 	if i <= 0 {
 		return fmt.Sprintf("%d", i)
@@ -61,4 +75,49 @@ func formatBytes(i Byte) string {
 	}
 
 	return ""
+}
+
+// Parses a postgres-like bytes string into Bytes
+func Parse(s string) (Byte, error) {
+
+	if len(strings.TrimSpace(s)) == 0 {
+		return 0, nil
+	}
+
+	lastDigit := 0
+	for _, r := range s {
+		if !(unicode.IsDigit(r)) {
+			break
+		}
+		lastDigit++
+	}
+
+	num := s[:lastDigit]
+
+	if len(strings.TrimSpace(num)) == 0 {
+		return 0, nil
+	}
+	f, err := strconv.ParseFloat(num, 64)
+	if err != nil {
+		return 0, fmt.Errorf("fail to parse float: %w", err)
+	}
+
+	unity := strings.ToLower(strings.TrimSpace(s[lastDigit:]))
+
+	return Byte(f) * extractBytesUnity(unity), nil
+}
+
+func extractBytesUnity(val string) Byte {
+	switch val {
+	case "kb":
+		return KB
+	case "mb":
+		return MB
+	case "gb":
+		return GB
+	case "tb":
+		return TB
+	default:
+		return B
+	}
 }
--- pkg/config/bytes_test.go
+++ pkg/config/bytes_test.go
@@ -1,57 +1,78 @@
 package config
 
 import (
-	"reflect"
+	"fmt"
 	"testing"
+
+	. "github.com/smartystreets/goconvey/convey"
 )
 
-func Test_marshalBytes(t *testing.T) {
-	tests := []struct {
-		name    string
-		args    Byte
-		want    []byte
-		wantErr bool
-	}{
-		{name: "should parse bytes to the postgres byte format",
-			args: 10 * GB,
-			want: []byte(`"10GB"`)},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			got, err := marshalBytes(&tt.args)
-			if (err != nil) != tt.wantErr {
-				t.Errorf("marshalBytes() error = %v, wantErr %v", err, tt.wantErr)
-				return
-			}
-			if !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("marshalBytes() = %v, want %v", string(got), string(tt.want))
-			}
+func Test_Bytes(t *testing.T) {
+	Convey("Parsing", t, func() {
+		Convey("should parse bytes to the postgres byte format", func() {
+			input := 10 * GB
+			got, err := marshalBytes(&input)
+			So(err, ShouldBeNil)
+			So(got, ShouldResemble, []byte(`"10GB"`))
 		})
-	}
-}
 
-func Test_formatBytes(t *testing.T) {
-	type args struct {
-		i int
-	}
-	tests := []struct {
-		name string
-		args Byte
-		want string
-	}{
-		{"none", -1, "-1"},
-		{"none", 0, "0"},
-		{"bytes", 5, "5B"},
-		{"kb", 455 * KB, "455KB"},
-		{"mb", 1023 * MB, "1023MB"},
-		{"gb", 565 * GB, "565GB"},
-		{"tb", 396 * TB, "396TB"},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := formatBytes(tt.args); got != tt.want {
-				t.Errorf("formatBytes() = %v, want %v", got, tt.want)
+		Convey("should format bytes to string", func() {
+			tests := []struct {
+				desc string
+				args Byte
+				want string
+			}{
+				{"negative values", -1, "-1"},
+				{"zero", 0, "0"},
+				{"Bytes", 5, "5B"},
+				{"KiloBytes", 455 * KB, "455KB"},
+				{"MegaBytes", 1023 * MB, "1023MB"},
+				{"GigaBytes", 565 * GB, "565GB"},
+				{"TeraBytes", 396 * TB, "396TB"},
+			}
+			for _, tt := range tests {
+				Convey(fmt.Sprintf("should format %s", tt.desc), func() {
+					got := FormatBytes(tt.args)
+					So(got, ShouldEqual, tt.want)
+				})
 			}
 		})
-	}
+
+		Convey("should parse bytes from string", func() {
+			Convey("should parse Bytes", func() {
+
+				got, err := Parse("5B")
+				So(err, ShouldBeNil)
+				So(got, ShouldEqual, 5)
+
+				got, err = Parse("5")
+				So(err, ShouldBeNil)
+				So(got, ShouldEqual, 5)
+			})
+			Convey("should parse KiloBytes", func() {
+
+				got, err := Parse("455KB")
+				So(err, ShouldBeNil)
+				So(got, ShouldEqual, 455*KB)
+			})
+			Convey("should parse MegaBytes", func() {
+
+				got, err := Parse("1023MB")
+				So(err, ShouldBeNil)
+				So(got, ShouldEqual, 1023*MB)
+			})
+			Convey("should parse GigaBytes", func() {
+
+				got, err := Parse("565GB")
+				So(err, ShouldBeNil)
+				So(got, ShouldEqual, 565*GB)
+			})
+			Convey("should parse TeraBytes", func() {
+
+				got, err := Parse("396TB")
+				So(err, ShouldBeNil)
+				So(got, ShouldEqual, 396*TB)
+			})
+		})
+	})
 }
